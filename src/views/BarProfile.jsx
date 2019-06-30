e48b24564616978e417c1af691e83344e31e32be
Vinicius Sampaio <vsampaio@ice.ufjf.br>
2019-06-30 20:25:23 -0300

    kk eae men

--- src/views/BarProfile.jsx
+++ src/views/BarProfile.jsx
@@ -4,30 +4,14 @@ import Card from "../components/Card/Card.jsx";
 import { Grid, Image } from "semantic-ui-react";
 import BaresDados from "../Dados/Bares.json";
 import Carousel from "semantic-ui-carousel-react";
+import DadosBares from "../Dados/Bares.json"
+import "./cssglobal.css";
 
-const elements = [
-  {
-    render: () => {
-      return (
-        <Image src="https://media-cdn.tripadvisor.com/media/photo-s/07/84/1d/64/sao-bartolomeu.jpg" />
-      );
-    }
-  },
-  {
-    render: () => {
-      return (
-        <Image src="https://media-cdn.tripadvisor.com/media/photo-f/07/84/1d/5d/sao-bartolomeu.jpg" />
-      );
-    }
-  },
-  {
-    render: () => {
-      return (
-        <Image src="https://media-cdn.tripadvisor.com/media/photo-f/07/84/1f/a4/arte-no-sao-bartolomeu.jpg" />
-      );
-    }
-  }
-];
+const elements = DadosBares[0].CarroselImages.map(imagemCarrosel => {
+  return ({
+    render: () => <Image src={imagemCarrosel} />
+  });
+});
 
 class BarProfile extends Component {
   render() {
@@ -40,7 +24,7 @@ class BarProfile extends Component {
           <i className="pe-7s-star" style={{ fontSize: "40px" }} />
         </div>
 
-        <Carousel
+        <Carousel className="ui.card"
           elements={elements}
           duration={3000}
           animation="slide left"
