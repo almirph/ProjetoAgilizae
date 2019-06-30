17d87762c3065e91e7a3957c26892547568a9967
Vinicius Sampaio <vsampaio@ice.ufjf.br>
2019-06-30 18:43:20 -0300

    adicionando campo de bar

--- src/views/Bares.jsx
+++ src/views/Bares.jsx
@@ -1,33 +1,18 @@
 import React, { Component } from "react";
 import Card from "../components/Card/Card.jsx";
+import BaresDados from "../Dados/Bares.json";
 
 export default class Bares extends Component {
   render() {
     return (
+
       <div style={{ padding: "50px" }}>
-        <Card
-          title="BAR TESTE "
-          category="coisas sobre o bar"
-          imagem="https://media-cdn.tripadvisor.com/media/photo-s/07/84/1d/64/sao-bartolomeu.jpg"
-        />
 
-        <Card
-          title="BAR TESTE "
-          category="coisas sobre o bar"
-          imagem="https://media-cdn.tripadvisor.com/media/photo-s/07/84/1d/64/sao-bartolomeu.jpg"
-        />
-
-        <Card
-          title="BAR TESTE "
-          category="coisas sobre o bar"
-          imagem="https://media-cdn.tripadvisor.com/media/photo-s/07/84/1d/64/sao-bartolomeu.jpg"
-        />
-
-        <Card
-          title="BAR TESTE "
-          category="coisas sobre o bar"
-          imagem="https://media-cdn.tripadvisor.com/media/photo-s/07/84/1d/64/sao-bartolomeu.jpg"
-        />
+        {BaresDados.map(bar => <Card
+          key={bar.key} title={bar.nome}
+          category={bar.descricao}
+          imagem={bar.imagem}
+        />)}
       </div>
     );
   }
